f50552137a6e068ef2aa7c171adf2a4f5e02d3fd
Andrew Shields <andrewshields@Andrews-iMac.attlocal.net>
2020-12-22 18:07:49 -0500

    compy swap

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import RecipieForm from "./components/RecipieForm";
+
+function App() {
+  return (
+    <div className="App Container">
+      <RecipieForm/>
+    </div>
+  );
+}
+
+export default App;
